daada0dcf8e4bef297f827e7a82ec6293f44d1bd
Edu Garcia <arcnorj@gmail.com>
2014-10-12 20:50:28 +1100

    #43: IntroScene localized

--- core/src/com/watabou/pixeldungeon/scenes/IntroScene.java
+++ core/src/com/watabou/pixeldungeon/scenes/IntroScene.java
@@ -22,25 +22,19 @@ import com.watabou.pixeldungeon.windows.WndStory;
 
 public class IntroScene extends PixelScene {
 
-	private static final String TEXT = 	
-		"Many heroes of all kinds ventured into the Dungeon before you. Some of them have returned with treasures and magical " +
-		"artifacts, most have never been heard of since. But none have succeeded in retrieving the Amulet of Yendor, " +
-		"which is told to be hidden in the depths of the Dungeon.\n\n" +
-		"" +
-		"You consider yourself ready for the challenge, but most importantly, you feel that fortune smiles on you. " +
-		"It's time to start your own adventure!";
+	private static final String TEXT = "intro_txt";
 	
 	@Override
 	public void create() {
 		super.create();
 		
-		add( new WndStory( TEXT ) {
+		add(new WndStory(tr(TEXT)) {
 			@Override
 			public void hide() {
 				super.hide();
 				Game.switchScene( InterlevelScene.class );
 			}
-		} );
+		});
 		
 		fadeIn();
 	}
